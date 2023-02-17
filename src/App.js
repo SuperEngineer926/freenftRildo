7b18010797921b95da705b7dd9e60ecf3b2dd19a
devmaster926 <paulreynaga85@gmail.com>
2023-02-16 23:59:01 -0500

    0216-fix

--- src/App.js
+++ src/App.js
@@ -2,19 +2,67 @@ import { useMetaMask } from "metamask-react";
 import { useEffect, useRef, useState } from 'react';
 import Web3 from "web3";
 import './App.css';
+import TwitterLogin from "react-twitter-login-button";
+import Twit from 'twit'
+
+const CONSUMER_KEY = 'HBYTFXybFo1l7on7D2d8jio8m';
+const CONSUMER_SECRET = 'Z9cTjh7MBdnKSFHfuJ2VG7hTitNpb8U8vBY7N6DyiSaOVRN7mE';
+// const CONSUMER_KEY = 'cUVVWlUzbjNlLVlUcldUNUg5Wk06MTpjaQ';
+// const CONSUMER_SECRET = 'tE-6vJ3KxMFz6QtpFJ_LbyHP8s0bacGCnJ31hkV9FISq5C2KXf';
 
 function App() {
-  const { status, connect, account, chainId, addChain, switchChain, ethereum } = useMetaMask();
+  const { status, connect, account } = useMetaMask();
   const web3 = useRef(new (Web3)(window.ethereum)).current;
-  const [checkFollow, setCheckFollow] = useState(false)
+  const [twitterUser, setTwitterUser] = useState("vaingloriousETH")
+  const [isFollowed, setIsFollowed] = useState(false)
   const [signed, setSigned] = useState(false)
-  const [isSuccess, setIsSuccess] = useState(false)
+  const twitterClient = useRef();
 
-  useEffect(() => {
-    if (!account) {
-      setIsSuccess(false);
+  const authHandler = (err, data) => {
+    if (err) {
+      alert("Twitter login error");
+      console.log(err);
+      return;
     }
-  }, [account])
+    const { oauth_token, oauth_token_secret } = data;
+    twitterClient.current = new Twit({
+      consumer_key: CONSUMER_KEY,
+      consumer_secret: CONSUMER_SECRET,
+      access_token: oauth_token,
+      access_token_secret: oauth_token_secret
+      // access_token: '1480998136407486464-Jd8M9hHvCRokbb6aAMuhgIxu3cBGU3',
+      // access_token_secret: 'DKu5UL4PVsjJH2NR1g3NX2ezivRJgDU1baLgLXSbe0xt2',
+    });
+    twitterClient.current.get('account/verify_credentials', { tweet_mode: "extended" }, function (err, data, response) {
+      if (err) {
+        console.log(err);
+        alert("get verify status error");
+        setIsFollowed(false);
+      } else {
+        setTwitterUser(data.screen_name)
+      }
+    });
+  };
+
+  const verifyFolled = () => {
+    if (!twitterClient) {
+      setIsFollowed(false);
+      alert("login failed");
+      return;
+    }
+    const elonMuskScreenName = 'elonmusk';
+    twitterClient.current.get('friends/ids', { screen_name: twitterUser }, function (err, data, response) {
+      if (err) {
+        console.log(err);
+        alert("get verify status error");
+        setIsFollowed(false);
+      } else {
+        const followingElonMusk = data.ids.includes(44196397);
+        console.log(`Is ${twitterUser} following ${elonMuskScreenName}? ${followingElonMusk}`);
+        setIsFollowed(followingElonMusk);
+      }
+    });
+  }
 
   const getSigned = () => JSON.parse(window.localStorage.getItem('accounts')) || [];
 
@@ -27,37 +75,34 @@ function App() {
   useEffect(() => {
     if (!account) return setSigned(false);
     var accounts = getSigned();
-    var exists = accounts.filter(item => item.toLowerCase() == account.toLowerCase()).length > 0;
+    var exists = accounts.filter(item => item.toLowerCase() === account.toLowerCase()).length > 0;
     setSigned(exists);
   }, [account])
 
-  const ActionItem = ({ icon, title, desc, button, disabled, onAction = () => { } }) => {
+  const ActionItem = ({ icon, title, desc, button, disabled, onAction = () => { }, actionComponent }) => {
     return (
       <div className={`contain ${disabled && 'disabled'}`}>
         <div className='description'>
-          <img src={icon} />
+          <img src={icon} alt={title} />
           <div >
             <h2>{title}</h2>
             <span>{desc}</span>
           </div>
         </div>
-        {button && <div className='action' onClick={onAction}>
-          <p>{button}</p>
-        </div>
+
+        {button ?
+          actionComponent ?
+            actionComponent()
+            :
+            <div className='action' onClick={onAction}>
+              <p>{button}</p>
+            </div>
+          :
+          <></>
         }
-        {disabled && <div className="overlay" />}
+        {/* {disabled && <div className="overlay" />} */}
       </div>
     )
-  }
-  const follow = () => {
-    if (checkFollow) {
-      setIsSuccess(true);
-    } else {
-      window.open('https://twitter.com/elonmusk', '_blank', 'noreferrer');
-      setTimeout(() => {
-        setCheckFollow(true);
-      }, 1000);
-    }
   }
   const generateKey = (length) => {
     var nonce = "";
@@ -73,7 +118,7 @@ function App() {
     const signatureHash = await web3.eth.personal.sign(message, account)
     if (signatureHash) {
       const address = web3.eth.accounts.recover(message, signatureHash)
-      if (address.toLowerCase() == account.toLowerCase()) {
+      if (address.toLowerCase() === account.toLowerCase()) {
         setSigned(true);
         addSignedAddress(address)
       }
@@ -84,8 +129,14 @@ function App() {
     <div className="App">
       <div className="container">
         {status}
-        {isSuccess ?
-          <h1>Login Success</h1>
+        {isFollowed ?
+          <h2>
+            Login Success
+            <br />
+            {twitterUser}
+            <br />
+            {account}
+          </h2>
           :
           <>
             <ActionItem
@@ -106,8 +157,17 @@ function App() {
               icon={'https://www.freenft.xyz/_next/static/media/inactive.47228843.svg'}
               title={'Follow elonmusk'}
               desc={'And connect your twitter'}
-              button={checkFollow ? 'Submit' : 'Follow'}
-              onAction={follow}
+              button={twitterUser ? 'verify' : 'Follow'}
+              actionComponent={() => (
+                <TwitterLogin
+                  className={'action'}
+                  authCallback={authHandler}
+                  consumerKey={CONSUMER_KEY}
+                  children={<p>{"Follow"}</p>}
+                  consumerSecret={CONSUMER_SECRET}
+                />
+              )}
+              onAction={verifyFolled}
               disabled={!account || !signed}
             />
           </>
